59014555f3de28a8684c23ff50a4cdd78b7bb156
Suhayl <suhaylibnamir@gmail.com>
2022-06-26 16:38:35 +0500

    comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constans.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,109 +9,201 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ArtApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ArtApp extends StatefulWidget {
+  const ArtApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ArtApp> createState() => _ArtAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _ArtAppState extends State<ArtApp> {
+  final Color scaffoldColor = const Color(0xfff8f8f8);
+  final Color textColor1 = const Color(0xff333333);
+  final Gradient _gradient = const LinearGradient(
+      colors: [Color(0xfff8f8f8), Color(0xff9F03FF)],
+      transform: GradientRotation(45));
+  String data =
+      "Together with my design team, we designed this futuristic Cyberyacht concept artwork. We wanted to create something that has not been created yet, so we started to collect ideas of how we imagine the Cyberyacht could look like in the future.";
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: scaffoldColor,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        backgroundColor: scaffoldColor,
+        elevation: 0,
+        title: Image.asset(
+          'assets/app_logo.png',
+          height: 37,
         ),
+        centerTitle: false,
+        actions: [
+          Row(
+            children: [
+                IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: textColor1,
+              size: 24,
+            ),
+          ),
+            IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu_outlined,
+              color: textColor1,
+              size: 24,
+            ),
+          ),
+            ],
+          )
+        
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+            child:Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Add wallet to pay',
+                      style: kTextStyle(
+                        color: textColor1,
+                        size: 20,
+                      ),
+                    ),
+                    Text(
+                      'Easy to sell your Digital Art with 3 step',
+                      style: kTextStyle(
+                          color: Colors.black54,
+                          size: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+            ),
+          ),
+          Container(
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextButton.icon(
+                  onPressed: () {},
+                  icon: Container(
+                    height: 100,
+                    width: 343,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Colors.black54,
+
+                    ),
+                    child: Text(
+                      '+',
+                      style: kTextStyle(
+                          size: 16,
+                          color: textColor1,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  label: Text(
+                    "Bank wallet",
+                    style: kTextStyle(
+                        size: 24,
+                        color: textColor1,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  style: buttonStyle(
+                    color: Colors.white,
+                    shadowColor: Colors.black12,
+                    elevation: 10,
+                    padding: const EdgeInsets.fromLTRB(5, 5, 15, 5),
+                    borderRadius: 20,
+                  ),
+                ),
+              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 40, left: 70, right: 70),
+            child: Image.asset(
+              "assets/app_logo.png",
+            ),
+          ),
+          Row(
+            children: [
+              Text(
+                "The",
+                style: kTextStyle(
+                    color: Colors.black45,
+                    size: 30,
+                    fontWeight: FontWeight.w200),
+              ),
+              Text(
+                "New",
+                style: kTextStyle(
+                    color: Colors.black54,
+                    size: 30,
+                    fontWeight: FontWeight.w300),
+              ),
+              Text(
+                "Creative",
+                style: kTextStyle(color: Colors.black87, size: 30),
+              ),
+              Text(
+                "Economy",
+                style: kTextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold, size: 30),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: 60,
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              gradient: _gradient,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Earn now',
+              style: kTextStyle(size: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Container(
+            height: 60,
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Color(0xff0000EA), width: 1),
+            ),
+            child: Text(
+              'Discover more',
+              style: kTextStyle(
+                  size: 20, fontWeight: FontWeight.w600, color: textColor1),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
     );
   }
 }
